e7cc52651f1459a9d2ea462f21f39c3e0e815392
Stanislav <handarulit@gmail.com>
2023-06-07 22:49:56 +0300

    add controllers for weather

---
+++ controllers/WeatherController.js
@@ -0,0 +1,30 @@
+const WeatherService = require('../services/WeatherService.js');
+
+
+
+class WeatherController {
+
+    async indexPage(req, res, next){
+        try {
+            
+            const weatherData = await WeatherService.getAll();
+            res.json(weatherData);
+        } catch (e) {
+            console.log(e);
+            next(e);
+        }
+    }
+
+    async weatherPage(req, res, next) {
+        try {
+            const town = req.query.town;
+            const weatherData = await WeatherService.getWeatherByTown(town);
+            res.send(weatherData);
+        } catch (e) {
+            console.log(e);
+            next(e);
+        }
+    }
+}
+
+module.exports = new WeatherController();
